a1b7008b59c2406b63fc0aef6bf7fdee58fc1345
kent <nwekep14@gmail.com>
2023-09-11 03:15:42 +0100

    remove functions to get and delete user by name

--- controller/userController.js
+++ controller/userController.js
@@ -61,24 +61,6 @@ exports.getUserById = (req, res, next) => {
 	});
 };
 
-exports.getUserByName = (req, res, next) => {
-	const { name } = req.params;
-
-	// Retrieve a user by id from the "users" table
-	const query = "SELECT * FROM users WHERE name = ?";
-	db.get(query, [name], (err, user) => {
-		if (err) {
-			return res.status(500).json({ error: "Error fetching user by name." });
-		}
-
-		if (!user) {
-			return res.status(404).json({ error: "User not found." });
-		}
-
-		res.status(200).json(user);
-	});
-};
-
 exports.updateUserName = (req, res, next) => {
 	const { id } = req.params;
 	const { name } = req.body;
@@ -106,19 +88,6 @@ exports.deleteUserById = (req, res, next) => {
 	// Delete the user from the "users" table by ID
 	const query = "DELETE FROM users WHERE id = ?";
 	db.run(query, [id], err => {
-		if (err) {
-			return res.status(500).json({ error: "Error deleting user." });
-		}
-		res.status(200).json({ message: "User deleted successfully." });
-	});
-};
-
-exports.deleteUserByName = (req, res, next) => {
-	const { name } = req.params;
-
-	// Delete the user from the "users" table by Name
-	const query = "DELETE FROM users WHERE name = ?";
-	db.run(query, [name], err => {
 		if (err) {
 			return res.status(500).json({ error: "Error deleting user." });
 		}
--- route/user.js
+++ route/user.js
@@ -10,9 +10,6 @@ router.get("/api", userControllers.getUsers);
 // get a user using the user id
 router.get("/api/:id", userControllers.getUserById);
 
-// optionally, get a user by using the user name
-router.get("/api/:name", userControllers.getUserByName);
-
 // create a new user
 router.post("/api", userControllers.createUser);
 
@@ -21,8 +18,5 @@ router.put("/api/:id", userControllers.updateUserName);
 
 // delete user using the user's id
 router.delete("/api/:id", userControllers.deleteUserById);
-
-// delete user using user's name
-router.delete("/api/:name", userControllers.deleteUserByName);
 
 module.exports = router;
